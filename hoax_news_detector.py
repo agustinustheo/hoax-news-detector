c9ba4d417dc892177283c00739e90960ab237ef1
Agustinus Theodorus <agustinustheoo@gmail.com>
2019-01-22 23:59:24 +0700

    managed to proccess web page text

--- hoax_news_detector.py
+++ hoax_news_detector.py
@@ -1,13 +1,22 @@
 import re
+import bs4
+import lxml
+import nltk
+import pickle
+import urllib.request
 from google import google
+from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 
 def preproccess_text(text_messages):
 	# change words to lower case - Hello, HELLO, hello are all the same word
 	processed = text_messages.lower()
 
+	# Remove remove unnecessary noise
+	processed = re.sub(r'\[[0-9]+\]|\[[a-z]+\]|\[[A-Z]+\]|\\\\|\\r|\\t|\\n|\\', ' ', processed)
+
 	# Remove punctuation
-	processed = re.sub(r'[.,\/#!%\^&\*;:|+{}=\-\'"_`~()?]', ' ', processed)
+	processed = re.sub(r'[.,\/#!%\^&\*;\[\]:|+{}=\-\'"_`~()?]', ' ', processed)
 
 	# Replace whitespace between terms with a single space
 	processed = re.sub(r'\s+', ' ', processed)
@@ -29,7 +38,7 @@ def news_title_tokenization(message):
 
 	return tokenized_news_title
 
-news_title = "donald trump government shutdown"
+news_title = "ahok bebas penjara"
 
 news_title_tokenized = news_title_tokenization(news_title)
 
@@ -69,3 +78,28 @@ if is_news_hoax > 4:
 	print('Not Fake News!')
 else:
 	print('Fake News!')
+
+def cleanMe(html):
+    soup = BeautifulSoup(html, features="lxml") # create a new bs4 object from the html data loaded
+    for script in soup(["script", "style"]): # remove all javascript and stylesheet code
+        script.extract()
+    # get text
+    text = soup.get_text()
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    return text
+
+webpage = str(urllib.request.urlopen('https://www.washingtonpost.com/powerpost/no-cave-trump-pelosi-vow-not-to-yield-in-government-shutdown-standoff/2019/01/22/1b6258bc-1e4b-11e9-8e21-59a09ff1e2a1_story.html?noredirect=on&utm_term=.2e3bfa71c3d5').read())
+cleantext = cleanMe(webpage)
+cleantext = re.sub(r'\\([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])\\([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])\\([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])([a-z]|[A-Z]|[0-9])', '', cleantext)
+# cleantext = preproccess_text(cleantext)
+
+# indonesian_sent_tokenizer_f = open("indonesian_sent_tokenizer.pickle", "rb")
+# indonesian_sent_tokenizer = pickle.load(indonesian_sent_tokenizer_f)
+# indonesian_sent_tokenizer_f.close()
+
+print(cleantext)
