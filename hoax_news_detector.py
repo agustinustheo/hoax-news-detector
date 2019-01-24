ab5df8e7e4594658eb6675a36990ce2e5932110c
agustinustheo <rickyph96>
2019-01-24 16:42:18 +0700

    remove unneccessary functions

--- hoax_news_detector.py
+++ hoax_news_detector.py
@@ -37,20 +37,6 @@ def remove_unnecessary_noise(text_messages):
 
 	return text_messages
 
-def clean_article(html):
-    soup = BeautifulSoup(html, features="lxml") # create a new bs4 object from the html data loaded
-    for script in soup(["script", "style"]): # remove all javascript and stylesheet code
-        script.extract()
-    # get text
-    text = soup.get_text()
-    # break into lines and remove leading and trailing space on each
-    lines = (line.strip() for line in text.splitlines())
-    # break multi-headlines into a line each
-    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
-    # drop blank lines
-    text = '\n'.join(chunk for chunk in chunks if chunk)
-    return text
-
 def news_title_tokenization(message):
 	tokenized_news_title = []
 	ps = PorterStemmer()
